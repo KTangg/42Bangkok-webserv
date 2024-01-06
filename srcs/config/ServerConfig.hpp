9966f3e542335ae28419b2a123cd70d12794b683
Samboon Poolprasart <k.tangtangtang@gmail.com>
2024-01-07 00:18:17 +0700

    ServerConfig: make canonical form class

--- srcs/config/ServerConfig.hpp
+++ srcs/config/ServerConfig.hpp
@@ -6,7 +6,7 @@
 /*   By: spoolpra <spoolpra@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 17:53:52 by spoolpra          #+#    #+#             */
-/*   Updated: 2024/01/06 16:52:20 by spoolpra         ###   ########.fr       */
+/*   Updated: 2024/01/07 00:16:55 by spoolpra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,18 +29,28 @@ namespace ft {
 
 class ServerConfig {
    public:
+    ServerConfig(const ServerConfig& src);
+    ServerConfig& operator=(const ServerConfig& rhs);
+    ~ServerConfig();
+
     ServerConfig(const std::map<std::string, Route>& routes,
                  const std::string&                  name = "",
                  const size_t&                       max_body_size = DEFAULT_MAX_BODY_SIZE,
                  const std::map<int, ErrorPage>&     error_pages = ft::initialize_error_pages());
-    ~ServerConfig();
 
     const std::string& get_name() const;
     const size_t&      get_max_body_size() const;
     const ErrorPage&   get_error_page(int code) const;
     const Route&       get_route(const std::string& path) const;
 
+    void set_name(const std::string& name);
+    void set_max_body_size(const size_t& max_body_size);
+    void set_error_pages(const std::map<int, ErrorPage>& error_pages);
+    void set_routes(const std::map<std::string, Route>& routes);
+
    private:
+    ServerConfig();
+
     std::string                  _name;
     size_t                       _max_body_size;
     std::map<int, ErrorPage>     _error_pages;
