cf92f01fb76cca0371a25d5ffa0f7cd07294dee5
Samboon Poolprasart <k.tangtangtang@gmail.com>
2024-01-09 15:14:23 +0700

    status_code: using define instead of hardcode http code

--- srcs/utils/status_code.cpp
+++ srcs/utils/status_code.cpp
@@ -6,52 +6,13 @@
 /*   By: spoolpra <spoolpra@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 21:36:45 by spoolpra          #+#    #+#             */
-/*   Updated: 2024/01/06 14:48:43 by spoolpra         ###   ########.fr       */
+/*   Updated: 2024/01/09 14:46:26 by spoolpra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils/status_code.hpp"
 
 namespace ft {
-    /**
-     * @brief Initialize the default error codes.
-     *
-     * @return const std::vector<int> The default error codes.
-     */
-    const std::vector<int> initialize_default_error_code() {
-        std::vector<int> default_error_code;
-
-        default_error_code.push_back(400);
-        default_error_code.push_back(401);
-        default_error_code.push_back(402);
-        default_error_code.push_back(403);
-        default_error_code.push_back(404);
-        default_error_code.push_back(405);
-        default_error_code.push_back(406);
-        default_error_code.push_back(407);
-        default_error_code.push_back(408);
-        default_error_code.push_back(409);
-        default_error_code.push_back(410);
-        default_error_code.push_back(411);
-        default_error_code.push_back(412);
-        default_error_code.push_back(413);
-        default_error_code.push_back(414);
-        default_error_code.push_back(415);
-        default_error_code.push_back(416);
-        default_error_code.push_back(417);
-        default_error_code.push_back(421);
-        default_error_code.push_back(422);
-        default_error_code.push_back(426);
-        default_error_code.push_back(500);
-        default_error_code.push_back(501);
-        default_error_code.push_back(502);
-        default_error_code.push_back(503);
-        default_error_code.push_back(504);
-        default_error_code.push_back(505);
-
-        return default_error_code;
-    }
-
     /**
      * @brief Initialize the http status codes.
      *
@@ -60,54 +21,52 @@ namespace ft {
     const std::map<int, std::string> initialize_http_status_codes() {
         std::map<int, std::string> http_status_codes;
 
-        http_status_codes[100] = "Continue";
-        http_status_codes[101] = "Switching Protocols";
-        http_status_codes[200] = "OK";
-        http_status_codes[201] = "Created";
-        http_status_codes[202] = "Accepted";
-        http_status_codes[203] = "Non-Authoritative Information";
-        http_status_codes[204] = "No Content";
-        http_status_codes[205] = "Reset Content";
-        http_status_codes[206] = "Partial Content";
-        http_status_codes[300] = "Multiple Choices";
-        http_status_codes[301] = "Moved Permanently";
-        http_status_codes[302] = "Found";
-        http_status_codes[303] = "See Other";
-        http_status_codes[304] = "Not Modified";
-        http_status_codes[305] = "Use Proxy";
-        http_status_codes[307] = "Temporary Redirect";
-        http_status_codes[400] = "Bad Request";
-        http_status_codes[401] = "Unauthorized";
-        http_status_codes[402] = "Payment Required";
-        http_status_codes[403] = "Forbidden";
-        http_status_codes[404] = "Not Found";
-        http_status_codes[405] = "Method Not Allowed";
-        http_status_codes[406] = "Not Acceptable";
-        http_status_codes[407] = "Proxy Authentication Required";
-        http_status_codes[408] = "Request Timeout";
-        http_status_codes[409] = "Conflict";
-        http_status_codes[410] = "Gone";
-        http_status_codes[411] = "Length Required";
-        http_status_codes[412] = "Precondition Failed";
-        http_status_codes[413] = "Content Too Large";
-        http_status_codes[414] = "URI Too Long";
-        http_status_codes[415] = "Unsupported Media Type";
-        http_status_codes[416] = "Range Not Satisfiable";
-        http_status_codes[417] = "Expectation Failed";
-        http_status_codes[421] = "Misdirected Request";
-        http_status_codes[422] = "Unprocessable Content";
-        http_status_codes[426] = "Upgrade Required";
-        http_status_codes[500] = "Internal Server Error";
-        http_status_codes[501] = "Not Implemented";
-        http_status_codes[502] = "Bad Gateway";
-        http_status_codes[503] = "Service Unavailable";
-        http_status_codes[504] = "Gateway Timeout";
-        http_status_codes[505] = "HTTP Version Not Supported";
+        http_status_codes[HTTP_CONTINUE] = "Continue";
+        http_status_codes[HTTP_SWITCHING_PROTOCOLS] = "Switching Protocols";
+        http_status_codes[HTTP_OK] = "OK";
+        http_status_codes[HTTP_CREATED] = "Created";
+        http_status_codes[HTTP_ACCEPTED] = "Accepted";
+        http_status_codes[HTTP_NON_AUTHORITATIVE_INFORMATION] = "Non-Authoritative Information";
+        http_status_codes[HTTP_NO_CONTENT] = "No Content";
+        http_status_codes[HTTP_RESET_CONTENT] = "Reset Content";
+        http_status_codes[HTTP_PARTIAL_CONTENT] = "Partial Content";
+        http_status_codes[HTTP_MULTIPLE_CHOICES] = "Multiple Choices";
+        http_status_codes[HTTP_MOVED_PERMANENTLY] = "Moved Permanently";
+        http_status_codes[HTTP_FOUND] = "Found";
+        http_status_codes[HTTP_SEE_OTHER] = "See Other";
+        http_status_codes[HTTP_NOT_MODIFIED] = "Not Modified";
+        http_status_codes[HTTP_USE_PROXY] = "Use Proxy";
+        http_status_codes[HTTP_TEMPORARY_REDIRECT] = "Temporary Redirect";
+        http_status_codes[HTTP_BAD_REQUEST] = "Bad Request";
+        http_status_codes[HTTP_UNAUTHORIZED] = "Unauthorized";
+        http_status_codes[HTTP_PAYMENT_REQUIRED] = "Payment Required";
+        http_status_codes[HTTP_FORBIDDEN] = "Forbidden";
+        http_status_codes[HTTP_NOT_FOUND] = "Not Found";
+        http_status_codes[HTTP_METHOD_NOT_ALLOWED] = "Method Not Allowed";
+        http_status_codes[HTTP_NOT_ACCEPTABLE] = "Not Acceptable";
+        http_status_codes[HTTP_PROXY_AUTHENTICATION_REQUIRED] = "Proxy Authentication Required";
+        http_status_codes[HTTP_REQUEST_TIMEOUT] = "Request Timeout";
+        http_status_codes[HTTP_CONFLICT] = "Conflict";
+        http_status_codes[HTTP_GONE] = "Gone";
+        http_status_codes[HTTP_LENGTH_REQUIRED] = "Length Required";
+        http_status_codes[HTTP_PRECONDITION_FAILED] = "Precondition Failed";
+        http_status_codes[HTTP_CONTENT_TOO_LARGE] = "Content Too Large";
+        http_status_codes[HTTP_URI_TOO_LONG] = "URI Too Long";
+        http_status_codes[HTTP_UNSUPPORTED_MEDIA_TYPE] = "Unsupported Media Type";
+        http_status_codes[HTTP_RANGE_NOT_SATISFIABLE] = "Range Not Satisfiable";
+        http_status_codes[HTTP_EXPECTATION_FAILED] = "Expectation Failed";
+        http_status_codes[HTTP_MISDIRECTED_REQUEST] = "Misdirected Request";
+        http_status_codes[HTTP_UNPROCESSABLE_CONTENT] = "Unprocessable Content";
+        http_status_codes[HTTP_UPGRADE_REQUIRED] = "Upgrade Required";
+        http_status_codes[HTTP_INTERNAL_SERVER_ERROR] = "Internal Server Error";
+        http_status_codes[HTTP_NOT_IMPLEMENTED] = "Not Implemented";
+        http_status_codes[HTTP_BAD_GATEWAY] = "Bad Gateway";
+        http_status_codes[HTTP_SERVICE_UNAVAILABLE] = "Service Unavailable";
+        http_status_codes[HTTP_GATEWAY_TIMEOUT] = "Gateway Timeout";
+        http_status_codes[HTTP_VERSION_NOT_SUPPORTED] = "HTTP Version Not Supported";
 
         return http_status_codes;
     }
-
-    const std::vector<int> default_error_code = initialize_default_error_code();
 
     const std::map<int, std::string> http_status_codes = initialize_http_status_codes();
 };  // namespace ft
--- srcs/utils/status_code.hpp
+++ srcs/utils/status_code.hpp
@@ -6,7 +6,7 @@
 /*   By: spoolpra <spoolpra@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 21:38:04 by spoolpra          #+#    #+#             */
-/*   Updated: 2024/01/06 14:40:59 by spoolpra         ###   ########.fr       */
+/*   Updated: 2024/01/09 15:13:51 by spoolpra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,54 @@
 #include <string>
 #include <vector>
 
+#define HTTP_CONTINUE 100
+#define HTTP_SWITCHING_PROTOCOLS 101
+#define HTTP_OK 200
+#define HTTP_CREATED 201
+#define HTTP_ACCEPTED 202
+#define HTTP_NON_AUTHORITATIVE_INFORMATION 203
+#define HTTP_NO_CONTENT 204
+#define HTTP_RESET_CONTENT 205
+#define HTTP_PARTIAL_CONTENT 206
+#define HTTP_MULTIPLE_CHOICES 300
+#define HTTP_MOVED_PERMANENTLY 301
+#define HTTP_FOUND 302
+#define HTTP_SEE_OTHER 303
+#define HTTP_NOT_MODIFIED 304
+#define HTTP_USE_PROXY 305
+#define HTTP_TEMPORARY_REDIRECT 307
+#define HTTP_BAD_REQUEST 400
+#define HTTP_UNAUTHORIZED 401
+#define HTTP_PAYMENT_REQUIRED 402
+#define HTTP_FORBIDDEN 403
+#define HTTP_NOT_FOUND 404
+#define HTTP_METHOD_NOT_ALLOWED 405
+#define HTTP_NOT_ACCEPTABLE 406
+#define HTTP_PROXY_AUTHENTICATION_REQUIRED 407
+#define HTTP_REQUEST_TIMEOUT 408
+#define HTTP_CONFLICT 409
+#define HTTP_GONE 410
+#define HTTP_LENGTH_REQUIRED 411
+#define HTTP_PRECONDITION_FAILED 412
+#define HTTP_CONTENT_TOO_LARGE 413
+#define HTTP_URI_TOO_LONG 414
+#define HTTP_UNSUPPORTED_MEDIA_TYPE 415
+#define HTTP_RANGE_NOT_SATISFIABLE 416
+#define HTTP_EXPECTATION_FAILED 417
+#define HTTP_MISDIRECTED_REQUEST 421
+#define HTTP_UNPROCESSABLE_CONTENT 422
+#define HTTP_UPGRADE_REQUIRED 426
+#define HTTP_INTERNAL_SERVER_ERROR 500
+#define HTTP_NOT_IMPLEMENTED 501
+#define HTTP_BAD_GATEWAY 502
+#define HTTP_SERVICE_UNAVAILABLE 503
+#define HTTP_GATEWAY_TIMEOUT 504
+#define HTTP_VERSION_NOT_SUPPORTED 505
 #define UNDEFINED_ERROR "Undefined error"
 
 namespace ft {
-    const std::vector<int>           initialize_default_error_code();
     const std::map<int, std::string> initialize_http_status_codes();
 
-    extern const std::vector<int>           default_error_code;
     extern const std::map<int, std::string> http_status_codes;
 }  // namespace ft
 
